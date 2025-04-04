d6517b3b1825d1b27b0a1d24001846b9917aa0c7
Cornel <corneliugutu69@gmail.com>
2025-04-04 21:08:10 +0300

    fix: changed street name in address sectin

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,17 +3,18 @@ import SocialLinks from "./SocialLinks";
 import PhoneLink from "./PhoneLink";
 
 const Footer = () => {
+  const street = "str. Mitropolit Dosoftei 89";
   return (
     <>
       <div className="hidden lg:flex items-center">
         <PhoneLink />
       </div>
       <div className="flex flex-col items-center justify-center text-sm lg:text-base border-neutral-700 border px-4 py-2 mb-2 rounded-xl font-medium">
-        {/* <p className="lg:hidden">Str. Mihail Sadoveanu 12</p> */}
+        <p className="lg:hidden">{street}</p>
         <p className="lg:hidden">Republic of Moldova, mun. Chisinau</p>
         <p className="hidden lg:flex">Republic of Moldova</p>
         <p className="hidden lg:flex">mun. Chisinau</p>
-        {/* <p className="hidden lg:flex">Str. Mihail Sadoveanu 12</p> */}
+        <p className="hidden lg:flex">{street}</p>
       </div>
       <div className="hidden lg:flex">
         <SocialLinks />
