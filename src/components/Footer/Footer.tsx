07ec4e8f00600045e895dfb3a8e236a3e867ac29
Corneliu Gutu <corneliugutu69@gmail.com>
2025-07-14 21:14:02 +0300

    style(footer): improved ui;

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,11 +7,11 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center text-sm mx-6 lg:text-base border-neutral-700 border-t px-4 py-2 mb-2 lg:mb-0 font-medium">
       <div className="pb-10 pt-4">{t("text")}</div>
-      <section className="flex justify-around w-2/3 mb-4">
-        <p className="hidden lg:flex">Mikea © 2025</p>
-        <p className="hidden lg:flex">{t("contact")}</p>
-        <a href={`tel:${phone}`} className="flex items-center">
-          <div className="hidden lg:flex ">{phone}</div>
+      <section className="flex text-xs md:text-base justify-between md:justify-around w-full md:w-2/3 mb-4">
+        <p className="">Mikea © 2025</p>
+        <p className="">{t("contact")}</p>
+        <a href={`tel:${phone}`} className="">
+          <div className="">{phone}</div>
         </a>
       </section>
     </footer>
